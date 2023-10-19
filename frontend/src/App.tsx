74abf03ca10b708a383f78b59bb24c9ce811aea1
Pat <mediodia.patrick@gmail.com>
2023-10-19 23:21:51 +0800

    Fixed CORS problem error and displayed initial get notes request

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,48 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Button } from 'react-bootstrap';
+import { Note } from './models/note';
 
 function App() {
   //[variable, function to change this variable], array destructuring
-  //set initial clickCount state to 0
+  //set initial clickCount state to empty list
   //always do this before the return function
-  const [clickCount, setClickCount] = useState(0);
+  const[notes, setNotes] = useState<Note[]>([]);
+
+  //execute side effects outside of rendering
+  useEffect(() => {
+
+    //use effect cannot take async functions
+    //so you add another function inside of it
+    async function loadNotes() {
+      try {
+
+        //fetch call to get notes
+        const response = await fetch("/api/notes", { method: "GET"});
+
+        //parse response to json
+        const notes = await response.json();
+        
+        //update the notes state
+        setNotes(notes);
+
+      } catch(error) {
+        console.log(error);
+        alert(error);
+      };
+    }
+
+    loadNotes();
+    
+    //make sure to pass a dependency array
+    //in this case it will only run once
+    //if you put variable to check here and hte variable value changes, it will trigger this useEffect
+  }, []);
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello, world!
-        </p>
-        <Button onClick={()=> setClickCount(clickCount + 1)}>
-          Clicked {clickCount} times
-        </Button>
-      </header>
+      {JSON.stringify(notes)}
     </div>
   );
 }
