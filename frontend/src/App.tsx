5e41d1dc537ee313f3c711bc6ecc8e646b791bd5
Pat <50007415+PatrickMediodia@users.noreply.github.com>
2023-10-26 17:55:09 +0800

    Added loading, error, and empty states

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import './styles/global.css';
-import { Button, Col, Container, Row } from 'react-bootstrap';
+import { Button, Col, Container, Row, Spinner } from 'react-bootstrap';
 import { Note as NoteModel } from './models/note';
 import Note from './components/Note';
 import styles from "./styles/NotesPage.module.css";
@@ -10,121 +10,143 @@ import AddEditNoteDialog from './components/AddEditNoteDialog';
 import { FaPlus } from "react-icons/fa"
 
 function App() {
-  //[variable, function to change this variable], array destructuring
-  //set initial clickCount state to empty list
-  //always do this before the return function
-  const[notes, setNotes] = useState<NoteModel[]>([]);
+	//[variable, function to change this variable], array destructuring
+	//set initial clickCount state to empty list
+	//always do this before the return function
+	const [notes, setNotes] = useState<NoteModel[]>([]);
+	
+	//states for the loading 
+	const [notesLoading, setNotesLoading] = useState<boolean>(true);
+	const [showNotesLoadingErorr, setShowNotesLoadingErorr] = useState<boolean>(false);
 
-  //state so that we know if we should show or hide the modal
-  const[showAddNoteDialog, setShowAddNoteDialog] = useState<boolean>(false);
+	//state so that we know if we should show or hide the modal
+	const [showAddNoteDialog, setShowAddNoteDialog] = useState<boolean>(false);
 
-  //keeps track of which note to edit when clicked
-  const[noteToEdit, setNoteToEdit] = useState<NoteModel|null>(null);
+	//keeps track of which note to edit when clicked
+	const [noteToEdit, setNoteToEdit] = useState<NoteModel|null>(null);
 
-  //execute side effects outside of rendering
-  useEffect(() => {
+	//execute side effects outside of rendering
+	useEffect(() => {
 
-    //use effect cannot take async functions
-    //so you add another function inside of it
-    async function loadNotes() {
-      try {
-        //get the notes from the wrapped fetch call
-        const notes = await NotesAPI.fetchNotes();
-        
-        //update the notes state
-        setNotes(notes);
+		//use effect cannot take async functions
+		//so you add another function inside of it
+		async function loadNotes() {
+			try {
+				//show loading indicator
+				setShowNotesLoadingErorr(false);
+				setNotesLoading(true);
 
-      } catch(error) {
-        console.log(error);
-        alert(error);
-      };
-    }
+				//get the notes from the wrapped fetch call
+				const notes = await NotesAPI.fetchNotes();
+				
+				//update the notes state
+				setNotes(notes);
+			} catch(error) {
+				console.log(error);
+				setShowNotesLoadingErorr(true);
+			} finally {
+				//hide loading indicator
+				setNotesLoading(false);
+			};
+		}
 
-    loadNotes();
+		loadNotes();
 
-    //make sure to pass a dependency array
-    //in this case it will only run once
-    //if you put variable to check here and hte variable value changes, it will trigger this useEffect
-  }, []); 
+		//make sure to pass a dependency array
+		//in this case it will only run once
+		//if you put variable to check here and hte variable value changes, it will trigger this useEffect
+	}, []); 
 
-  async function deleteNote(note: NoteModel) {
-    try {
-      //call the delete function in network notes_api
-      await NotesAPI.deleteNote(note._id);
-      
-      //filter the notes to check if id of note is id to not include
-      setNotes(notes.filter(existingNote => existingNote._id !== note._id));
-    } catch (error) {
-      console.log(error);
-      alert(error);
-    }
-  }
+	async function deleteNote(note: NoteModel) {
+		try {
+			//call the delete function in network notes_api
+			await NotesAPI.deleteNote(note._id);
+			
+			//filter the notes to check if id of note is id to not include
+			setNotes(notes.filter(existingNote => existingNote._id !== note._id));
+		} catch (error) {
+			console.log(error);
+		alert(error);
+		}
+	}
 
-  return (
-    <Container>
-      <Button
-        className={`mb-4 ${styleUtils.blockCenter} ${styleUtils.flexCenter}`}
-        onClick={()=> setShowAddNoteDialog(true)} >
-        <FaPlus />
-        Add new note
-      </Button>
+	const notesGrid = 
+		<Row xs={1} md={2} xl={3} className={`g-4 ${styles.noteGrid}`}>
+			{
+				notes.map(note=> (
+					<Col key={note._id}>
+					<Note 
+						note={note} 
+						onNoteClicked={setNoteToEdit} 
+						className={styles.note}
+						onDeleteNoteClicked={deleteNote}
+					/>
+					</Col>
+				))
+			}
+		</Row>
 
-      <Row xs={1} md={2} xl={3} className="g-4">
-        {
-          notes.map(note=> (
-            <Col key={note._id}>
-              <Note 
-                note={note} 
-                onNoteClicked={setNoteToEdit} 
-                className={styles.note}
-                onDeleteNoteClicked={deleteNote}
-              />
-            </Col>
-          ))
-        }
-      </Row>
+	return (
+		<Container className={styles.notePage}>
+			<Button
+				className={`mb-4 ${styleUtils.blockCenter} ${styleUtils.flexCenter}`}
+				onClick={()=> setShowAddNoteDialog(true)} >
+				<FaPlus />
+				Add new note
+			</Button>
 
-      {
-        //like a ternary operator, only show AddNoteDIalog if it is true
-        //state is not passed as a prop because its state when closed will be retained
-        // for example if they closed it, its contents will stay
-        //in the current configuration it will clear the state altogether
+			{/* show loading indicator if notes loading is true */ }
+			{ notesLoading && <Spinner animation='border' variant='primary' /> }
 
-        showAddNoteDialog && 
-          <AddEditNoteDialog 
-            onDismiss={ ()=> setShowAddNoteDialog(false) }
-            onNoteSaved={ (newNote) => {
-                /* ...notes is the spread function */
-                setNotes([...notes, newNote]);
-                setShowAddNoteDialog(false);
-              }
-            }
-          />
-        
-        //onDismiss as a prop drills down the setShowAddNoteDialog to be executed onHide
-      }
+			{/* show the paragraph tag if showNotesLoading error is true */ }
+			{ showNotesLoadingErorr && <p>Something went wrong. Please refresh the page.</p> }
 
-      {
-        noteToEdit &&
-          <AddEditNoteDialog
-            onDismiss={ ()=> setNoteToEdit(null) }
-            noteToEdit={noteToEdit}
-            onNoteSaved={ (updatedNote) => {
-                /* check if note is the same id as updated note, if same, use the updated note instead */
-                setNotes(
-                  notes.map(note =>
-                    note._id === updatedNote._id 
-                      ? updatedNote
-                      : note
-                  )
-                );
-                setNoteToEdit(null);
-              }
-            }
-        />
-      }
-    </Container>
-  );
+			{/* if length of notes state is not zero, show grid, else show message */}
+			{ !notesLoading && !showNotesLoadingErorr &&
+				<>
+					{ notes.length > 0 ? notesGrid : <p>You don't have any notes yet</p> }
+				</>
+			}
+
+			{
+				//like a ternary operator, only show AddNoteDIalog if it is true
+				//state is not passed as a prop because its state when closed will be retained
+				// for example if they closed it, its contents will stay
+				//in the current configuration it will clear the state altogether
+				showAddNoteDialog && 
+				<AddEditNoteDialog 
+					onDismiss={ ()=> setShowAddNoteDialog(false) }
+					onNoteSaved={ (newNote) => {
+						/* ...notes is the spread function */
+						setNotes([...notes, newNote]);
+						setShowAddNoteDialog(false);
+					}
+					}
+				/>
+				//onDismiss as a prop drills down the setShowAddNoteDialog to be executed onHide
+			}
+
+			{
+				noteToEdit &&
+				<AddEditNoteDialog
+					onDismiss={ ()=> setNoteToEdit(null) }
+					noteToEdit={noteToEdit}
+					onNoteSaved={ (updatedNote) => {
+						/* check if note is the same id as updated note, if same, use the updated note instead */
+						setNotes(
+							notes.map(note =>
+								note._id === updatedNote._id 
+								? updatedNote
+								: note
+							)
+						);
+						setNoteToEdit(null);
+						}
+					}
+				/>
+			}
+		</Container>
+	);
 }
 
 export default App;
